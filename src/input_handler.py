04c09ad911d1afb832239ab5168adea705f2f46e
William L <41084946+helveticae@users.noreply.github.com>
2023-08-27 13:06:16 +0200

    add boiler InputHandler, add basic player_controls

---
+++ src/input_handler.py
@@ -0,0 +1,29 @@
+import pygame
+
+
+class InputHandler:
+    def __init__(self):
+        self.keys = {}
+
+    def update(self):
+        self.keys = pygame.key.get_pressed()
+
+    def is_key_pressed(self, key):
+        return self.keys[key]
+
+
+def player_controls(player):
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_w]:
+        player.move("up")
+    if keys[pygame.K_s]:
+        player.move("down")
+    if keys[pygame.K_a]:
+        player.move("left")
+    if keys[pygame.K_d]:
+        player.move("right")
+    if keys[pygame.K_q]:
+        player.move("forward")
+    if keys[pygame.K_e]:
+        player.move("backward")
